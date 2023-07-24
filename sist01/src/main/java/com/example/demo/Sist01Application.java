8892c5208aa5dd1ad07dc41dc67b9b75047547eb
sist0821 <sist08212023@gmail.com>
2023-07-24 12:23:06 +0900

    first commit

---
+++ sist01/src/main/java/com/example/demo/Sist01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sist01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sist01Application.class, args);
+	}
+
+}
